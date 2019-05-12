cf8bb3504b9fa86682dd880d786aa7dd472aebaa
CORP\mhungcam <marco-luis.hung-campos@sogeti.com>
2019-05-12 12:38:41 +0200

    Install bootstrap awesome font and change the app template to inline template

--- Final/mena-vademecum/src/app/app.component.ts
+++ Final/mena-vademecum/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div><h1>{{title}}</h1>
+  <div>My first Component</div>
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
