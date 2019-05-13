5a1513c7b1be497919c55d8becedecbe9f57ba09
CORP\mhungcam <marco-luis.hung-campos@sogeti.com>
2019-05-13 10:39:12 +0200

    Removed   selector: 'app-medicine-detail' that is not necessary

--- Final/mena-vademecum/src/app/medicines/medicine-detail.component.ts
+++ Final/mena-vademecum/src/app/medicines/medicine-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-medicine-detail',
+
   templateUrl: './medicine-detail.component.html',
   styleUrls: ['./medicine-detail.component.css']
 })
