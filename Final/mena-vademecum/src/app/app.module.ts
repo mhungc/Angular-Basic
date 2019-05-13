459583001173740deade4f1866fd03ba02118705
CORP\mhungcam <marco-luis.hung-campos@sogeti.com>
2019-05-13 18:07:22 +0200

    Added shared module and other feature module

--- Final/mena-vademecum/src/app/app.module.ts
+++ Final/mena-vademecum/src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { MedicineListComponent } from './medicines/medicine-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MedicineDetailComponent } from './medicines/medicine-detail.component';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
+import { MedicineModule } from './medicines/medicine.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MedicineListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    MedicineDetailComponent,
     WelcomeComponent
   ],
   imports: [
@@ -25,12 +19,11 @@ import { WelcomeComponent } from './home/welcome.component';
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'medicines', component: MedicineListComponent },
-      { path: 'medicines/:id', component: MedicineDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ]),
+    MedicineModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
