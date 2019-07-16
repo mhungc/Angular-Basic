97996dc7f36de9e9b0484c35c08edd5037e8677e
CORP\mhungcam <marco-luis.hung-campos@sogeti.com>
2019-07-16 13:03:48 +0200

    Added Final Advance course folder

---
+++ FinalAdvance/mena-vademecum/src/app/medicines/medicine.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MedicineListComponent } from './medicine-list.component';
+import { MedicineDetailComponent } from './medicine-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    MedicineListComponent,
+    MedicineDetailComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+
+    RouterModule.forChild([
+      { path: 'medicines', component: MedicineListComponent },
+      { path: 'medicines/:id', component: MedicineDetailComponent }
+    ]),
+    SharedModule,
+  ]
+})
+export class MedicineModule { }
